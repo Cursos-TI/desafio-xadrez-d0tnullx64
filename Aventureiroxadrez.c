ad909a113e40cbfde3053129a93e78600f35f5c6
d0tnullx64 <neoawry@gmail.com>
2025-04-02 14:14:44 -0300

    Update Aventureiroxadrez.c

--- Aventureiroxadrez.c
+++ Aventureiroxadrez.c
@@ -1,33 +1,42 @@
 #include <stdio.h>
+#define MOV_TORRE 5
+#define MOV_BISPO 5
+#define MOV_RAINHA 8
+#define MOV_CAVALO_VERT 2
+#define MOV_CAVALO_HORIZ 1
 
 int main() {
-    // Torre
-    for (int i = 0; i < 5; i++) {
+    // Torre - for
+    for (int i = 0; i < MOV_TORRE; i++) {
         printf("Direita\n");
     }
     printf("\n");
 
-    // Bispo
-    int contador = 0;
-    while (contador < 5) {
+    // Bispo - while
+    int mov_bispo = 0;
+    while (mov_bispo < MOV_BISPO) {
         printf("Cima, Direita\n");
-        contador++;
+        mov_bispo++;
     }
     printf("\n");
 
-    // Rainha
-    contador = 0;
+    // Rainha - do-while
+    int mov_rainha = 0;
     do {
         printf("Esquerda\n");
-        contador++;
-    } while (contador < 8);
+        mov_rainha++;
+    } while (mov_rainha < MOV_RAINHA);
     printf("\n");
 
-    // Cavalo - movimento em L
-    for (int i = 0; i < 2; i++) {
+    // Cavalo - loops aninhados (for + while)
+    for (int i = 0; i < MOV_CAVALO_VERT; i++) {
         printf("Baixo\n");
     }
-    printf("Esquerda\n");
+    int mov_lateral = 0;
+    while (mov_lateral < MOV_CAVALO_HORIZ) {
+        printf("Esquerda\n");
+        mov_lateral++;
+    }
 
     return 0;
 }
